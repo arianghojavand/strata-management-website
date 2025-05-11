3595d9db44023b88d306dbfe7d681dcc8c5e87c4
arian ghojavand <arianghojavand@gmail.com>
2025-05-11 21:51:38 +1000

    temporarily changing middleware time-out hours

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,6 +1,6 @@
 export function middleware(request) {
   const hour = new Date().getHours();
-  if (hour >= 3 && hour < 4) {
+  if (hour >= 19 && hour < 23) {
     return new NextResponse("We're sleeping! Try again after 5am.", { status: 503 });
   }
 
